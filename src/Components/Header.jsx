a2edc2fdcff128285fb7f9b69c7132250f2b7ebc
Leonardo Morselli <leonardo.morselli02@gmail.com>
2023-12-01 13:57:27 +0100

    feat(header): header iniziato, da cambiare il colore alle immagini

---
+++ src/Components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Flex, Text, Box } from "@mantine/core";
+import  unity  from "../Logos/unity.svg";
+import unreal from "../Logos/unreal-engine.svg";
+import github from "../Logos/github.svg";
+import react from "../Logos/react.svg"
+
+export default function Header() {
+    return (
+        <Box className="bg-light2 pt-9 pb-9">
+            <Box className="mx-auto max-w-6xl flex">
+                <img src={react} className="mx-auto h-24 w-24"/>
+                <img src={github} className="mx-auto h-24 w-24"/>
+                <Flex direction={"column"} className="mx-auto max-w-xl border-solid border-2 border-dark2 p-5 text-center">
+                    <Text className="text-3xl text-dark2">Leonardo Morselli</Text>
+                    <Text className="text-2xl text-dark2">Full-Stack Developer | Gameplay Programmer</Text>
+                </Flex>
+                <img src={unity} className="mx-auto h-24 w-24" />
+                <img src={unreal} className="mx-auto h-24 w-24" />
+            </Box>
+        </Box>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {},
+    colors: {
+      'light1': "#FCDC4D",
+      'light2': "#CB793A",
+      'mid': "#9A031E",
+      'dark1': "#5F0F40",
+      'dark2': "#321325"    
+    }
+  },
+  plugins: [],
+}
+
